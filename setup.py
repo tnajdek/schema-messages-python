8bceb9aed7f4ffe5018404b8a02893a19ba622ad
Tom Najdek <tom@doppnet.com>
2015-07-18 20:11:26 +0100

    Tweaking layout

---
+++ setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'schema-messages'
+    'description': '''Simple library that lets you create binary
+        representations of arbitrary messages based on a schema
+        provided.
+
+        Anticipated for use in applications where identical structure
+        messages are transmitted repeatively.
+        ''',
+    'author': 'Tom Najdek',
+    'url': 'https://github.com/tnajdek/schema-messages-python',
+    'author_email': 'tom@doppnet.com',
+    'version': '0.1',
+    'packages': ['message']
+}
+
+setup(**config)
