d9789a9d43e3031a74509dab523025c85eebfa85
Tom Najdek <tom@doppnet.com>
2015-07-19 15:24:34 +0100

    License, packaging

--- setup.py
+++ setup.py
@@ -5,18 +5,30 @@ except ImportError:
 
 config = {
     'name': 'schema-messages',
-    'description': '''Simple library that lets you create binary
-        representations of arbitrary messages based on a schema
-        provided.
-
-        Anticipated for use in applications where identical structure
-        messages are transmitted repeatively.
-        ''',
+    'description': ''''Library to create network-efficient binary
+        representations of structured data.''',
+    'long_description': '''Schema Messages creates binary representation of
+        structured data that can be efficiently transmitted over network.
+        Anticipated for use in applications where identical structure messages
+        are transmitted repeatively, e.g. in multiplayer/online games.''',
+    'license': 'MIT',
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     'author': 'Tom Najdek',
     'url': 'https://github.com/tnajdek/schema-messages-python',
     'author_email': 'tom@doppnet.com',
-    'version': '0.1',
-    'packages': ['schemamessages']
+    'version': '0.1.0',
+    'packages': ['schemamessages'],
+    'install_requires': ['future', 'bidict']
 }
 
 setup(**config)
